6c658b2f98f6a312152e57db773665e29c0749c5
idklolxd <kieranckeith@gmail.com>
2017-09-04 14:26:28 -0600

    command testing

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ const discord = require('discord.js');
 const client = new discord.Client();
 
 client.on('ready', () => {
-    client.user.setGame(client.guilds.size + ` servers | ` + `Do $commands`)
+    client.user.setGame(client.guilds.size + ` servers | ` + `Do $help`)
 });
 
+client.on('message', message => {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+  
+    let command = message.content.split(" ")[0]
+    command = command.slice(prefix.length)
+    console.log(command);
+  
+    let args = message.content.split(" ").slice(1);
+});
 
 client.login('MzU0MzU4MDA3Mzc2NjQyMDQ4.DI9FSg.7S8rB-lJ0WUBGcR6Rwga8LHO8gw');
