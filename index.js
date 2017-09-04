671f745ae7de7f83e6e9d0cf012ef8cf87cb12f7
idklolxd <kieranckeith@gmail.com>
2017-09-04 14:21:12 -0600

    Discord commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     client.user.setGame(client.guilds.size + ` servers | ` + `Do $commands`)
-  });
+});
 
 
 client.login('MzU0MzU4MDA3Mzc2NjQyMDQ4.DI9FSg.7S8rB-lJ0WUBGcR6Rwga8LHO8gw');
