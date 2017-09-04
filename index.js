664d00939c316c05b6150fa07181d45eefe825b8
idklolxd <kieranckeith@gmail.com>
2017-09-04 17:06:14 -0600

    node

--- index.js
+++ index.js
@@ -1,36 +1,49 @@
 const discord = require('discord.js');
-const client = new discord.Client();
-const prefix = "$"
 
-client.on('ready', () => {
-    client.user.setGame(client.guilds.size + ` servers | ` + `Do $help`)
+const TOKEN = "MzU0MzU4MDA3Mzc2NjQyMDQ4.DI9FSg.7S8rB-lJ0WUBGcR6Rwga8LHO8gw"
+const PREFIX = "$"
+
+var ball = ['Yes',
+'No doubt about it',
+'Try again','signs point to yes',
+'I say no',
+'No chance',
+'Dont think so'];
+
+const bot = new discord.Client();
+
+//ffmpeg –version
+
+bot.on('ready', function() {
+    bot.user.setGame(bot.guilds.size + ` servers | ` + `Do $help`)
+    console.log('Ready!')
 });
 
-client.on('message', message => {
-    if (message.author.bot) return;
-    if (!message.content.startsWith(prefix)) return;
-  
-    let command = message.content.split(" ")[0]
-    command = command.slice(prefix.length)
-    console.log(command);
-  
-    let args = message.content.split(" ").slice(1);
+bot.on('message', function(message) {
+    if (message.author.equals(bot.user)) return;
+    if (!message.content.startsWith(PREFIX)) return;
 
-    if (command === "ping") {
-        message.channel.sendMessage("pong");
-    } 
-    if (command === "pong") {
-        message.channel.sendMessage("ping");
+    var args = message.content.substring(PREFIX.length).split(" ");
+
+    switch (args[0].toLowerCase()) {
+        case "ping":
+            message.channel.send('pong!');
+            break;
+        case "info":
+            message.channel.send('I\'m a bot created by Alumark.  ' + PREFIX + 'help for more commands.');
+            break;
+        case "8ball":
+            message.channel.send(ball[Math.floor(Math.random () * ball.length)]);
+            break;
+        case "help":
+        let embed = new discord.RichEmbed()
+        .setColor(0x2ECC71)
+        .addField('Commands:', `UberBot`)
+        .addField('ping:', `sends "pong!" \n Usage: $ping`)
+            break;
+        default:
+            message.channel.send('Invalid Command.')
     }
-
 });
 
-function clean(text) {
-    if (typeof (text) === "string")
-      return text.replace(/`/g, "`" + String.fromCharCode(8203))
-      .replace(/@/g, "@" + String.fromCharCode(8203));
-    else
-      return text;
-  }
-
-client.login('MzU0MzU4MDA3Mzc2NjQyMDQ4.DI9FSg.7S8rB-lJ0WUBGcR6Rwga8LHO8gw');
+bot.login(TOKEN);
